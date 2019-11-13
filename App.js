9cbe65e04b6f58e8f4be183b5b8173b8be461729
zyunique <335454895@qq.com>
2019-11-13 15:23:41 +0800

    update

---
+++ App.js
@@ -0,0 +1,104 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {NativeModules} from 'react-native';
+const BarcodeReaderManager = NativeModules.BarcodeReaderManager;
+
+const App : () => React$Node = () => {
+  state = {
+    result: 'N/A' 
+  };
+  onButtonPress = () => {
+    BarcodeReaderManager.readBarcode('your license here').then((events) =>{
+      // this.setState({result: events});
+      alert(events)
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <View style={styles.container}>
+        <Button title='Read Barcode' onPress={this.onButtonPress.bind(this)} />
+        <Text style={styles.display}>
+          Barcode Result : {this.state.result}
+        </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  display: {
+    fontSize: 20,
+    textAlign: 'center',
+    color:'#FFD700',
+    margin: 10,
+  },
+});
+
+export default App;
--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-import {NativeModules} from 'react-native';
-module.exports = NativeModules.BarcodeReaderManager;
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () => App);
