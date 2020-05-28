1f984c1531108b7a4f899a77ab0c33ccaf417be1
zyunique <335454895@qq.com>
2020-05-28 15:27:20 +0800

    rm

--- Libraries/ResultViewController.m
+++
@@ -1,9 +0,0 @@
-//
-//  ResultViewController.m
-//  BarcodeReaderManager
-//
-//  Created by Bob on 2020/5/26.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
